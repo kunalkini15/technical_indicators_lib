5e5f5bc6daabd7ebac11b7318c499ea5764b65dc
kunalkini15 <kunalkini15@gmail.com>
2020-08-05 20:22:23 +0530

    updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,11 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+# from technical_indicators_lib import *
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-from technical_indicators_lib import *
 # -- Project information -----------------------------------------------------
 
 project = 'Technical Indicators'
